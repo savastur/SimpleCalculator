8cbcefd910cce275b01070bdbea2bf5903010620
Sturm <savagesturm@gmail.com>
2026-01-25 16:38:02 -0700

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type First Number");
+            int res, a, b;
+            String SignInput = "";
+
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type second number");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Slect Sign:(/,*,+,-)");
+
+
+            SignInput = Console.ReadLine();
+            switch (SignInput)
+            {
+                                case "/":
+                    res = a / b;
+                    Console.WriteLine("Result:" + res);
+                    Console.WriteLine($" {res = a / b};"); 
+                    
+                    break;
+                case "+":
+                    res = a + b;
+                    Console.WriteLine("Result:" + res); 
+                    Console.WriteLine($"{res = a + b}");
+                    break;
+                    case "-":
+                    res = a - b;
+                    Console.WriteLine("Result:" + res); 
+                    Console.WriteLine($"{res = a - b}");
+                    break;
+                case "*":
+                    res = a * b;
+                    Console.WriteLine("Result:" + res); 
+                    Console.WriteLine($"{res = a * b}");
+                    break;
+            }
+            Console.Read();
+
+            }
+
+            
+          
+            
+
+
+
+
+           
+        
+
+        private static int Consolereadline()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
